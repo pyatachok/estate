1b4071802b308647915b953e6142f781d74b9dc9
pyatachok <pyatachok@pn1>
2013-05-12 14:14:26 +0300

    adding entity publisher + setters + configure database connection. php app/console doctrine:generate:entities AdManager/PublisherBundle/Entity/Publisher

---
+++ src/AdManager/PublisherBundle/Entity/Publisher.php
@@ -0,0 +1,62 @@
+<?php
+// src/AdManager/PublisherBundle/Entity/Publisher.php
+namespace AdManager\PublisherBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="publisher")
+ */
+class Publisher
+{
+    
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var type 
+     */
+    protected $name;
+
+    
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Publisher
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
