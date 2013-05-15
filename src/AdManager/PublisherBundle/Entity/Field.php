13d38aafca232903564d1f3baccb1229ff236001
pyatachok <pyatachok@pn1>
2013-05-15 20:38:47 +0300

    добавлена сущность поля getters and setters php app/console doctrine:generate:entities AdManager/PublisherBundle/Entity/Field

--- src/AdManager/PublisherBundle/Entity/Field.php
+++ src/AdManager/PublisherBundle/Entity/Field.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Validator\Mapping\ClassMetadata;
 use Symfony\Component\Validator\Constraints\NotBlank;
-
+use AdManager\PublisherBundle\Entity\Field;
 /**
  * @ORM\Entity
  * @ORM\Table(name="field")
@@ -28,6 +28,50 @@ class Field
      */
     protected $name;
 
+    
+    
+    /**
+     * @ORM\ManyToMany(targetEntity="Field", mappedBy="relatedFields")
+     **/
+    private $parentFields;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="Field", inversedBy="parentFields")
+     * @ORM\JoinTable(name="related_fields",
+     *      joinColumns={@ORM\JoinColumn(name="field_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="related_field_id", referencedColumnName="id")}
+     *      )
+     **/
+    private $relatedFields;
+    
+    public function __construct() {
+        $this->parentFields = new ArrayCollection();
+        $this->relatedFields = new ArrayCollection();
+    }
+
+    /**
+     * Get parent fields
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getParentFields()
+    {
+        return $this->parentFields;
+    }
+    
+    /**
+     * Get relatedFields
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getRelatedFields()
+    {
+        return $this->relatedFields;
+    }
+    
+    
+    
+    
     
     /**
      * Get id
@@ -66,5 +110,51 @@ class Field
     {
         $metadata->addPropertyConstraint('name', new NotBlank());
 
+    }
+
+    /**
+     * Add parentFields
+     *
+     * @param \AdManager\PublisherBundle\Entity\Field $parentFields
+     * @return Field
+     */
+    public function addParentField(\AdManager\PublisherBundle\Entity\Field $parentFields)
+    {
+        $this->parentFields[] = $parentFields;
+    
+        return $this;
+    }
+
+    /**
+     * Remove parentFields
+     *
+     * @param \AdManager\PublisherBundle\Entity\Field $parentFields
+     */
+    public function removeParentField(\AdManager\PublisherBundle\Entity\Field $parentFields)
+    {
+        $this->parentFields->removeElement($parentFields);
+    }
+
+    /**
+     * Add relatedFields
+     *
+     * @param \AdManager\PublisherBundle\Entity\Field $relatedFields
+     * @return Field
+     */
+    public function addRelatedField(\AdManager\PublisherBundle\Entity\Field $relatedFields)
+    {
+        $this->relatedFields[] = $relatedFields;
+    
+        return $this;
+    }
+
+    /**
+     * Remove relatedFields
+     *
+     * @param \AdManager\PublisherBundle\Entity\Field $relatedFields
+     */
+    public function removeRelatedField(\AdManager\PublisherBundle\Entity\Field $relatedFields)
+    {
+        $this->relatedFields->removeElement($relatedFields);
     }
 }
